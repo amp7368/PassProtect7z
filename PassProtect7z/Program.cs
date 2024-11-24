76929d8d6a493138625f669757a7ff108fd097e0
Aaron Peterham <47309189+amp7368@users.noreply.github.com>
2024-11-24 16:33:18 -0500

    - More rigourous archive testing for validity - Add cache using CRC32 to check if archive matches

--- PassProtect7z/Program.cs
+++ PassProtect7z/Program.cs
@@ -1,28 +1,67 @@
-﻿namespace PassProtect7z {
+﻿using PassProtect7z.Cache;
+using System.Text;
+
+namespace PassProtect7z {
     class Program {
-        public static string Password;
+        public static string Password = "";
         public static string ROOT_DIR;
         public static string CONFIG_FILE_NAME = "config.json";
+        private static List<string> errors = new();
+
         static void Main(string[] args) {
             try {
                 Run(args);
                 Console.WriteLine("Done!!!");
-                Console.ReadKey();
             } catch (Exception ex) {
+                Console.WriteLine();
                 Console.WriteLine(ex.Message);
                 Console.WriteLine(ex.StackTrace);
-                Console.Write("Press a key to continue.");
-                Console.ReadKey();
-                throw;
+            } finally {
+                Console.WriteLine();
+                Console.WriteLine("Errors:");
+                for (int i = 0; i < errors.Count; i++) {
+                    Console.WriteLine($"{i + 1}. {errors[i]}");
+                }
             }
+            Console.WriteLine();
+            Console.WriteLine("Press a enter to continue.");
+            Console.ReadLine();
         }
+        static void VerifyPassword() {
+            int remainingTries = 5;
+            while (remainingTries != 0) {
+                Password = ReadPassword();
+                if (Password.Length == 0) {
+                    Console.WriteLine("Password is empty. Try again...");
+                    continue;
+                }
 
-        static void PrepEnv() {
+                if (ProgramConfig.get().PasswordMatches(Password)) {
+                    Console.WriteLine("Passwords match! Starting now.");
+                    return;
+                }
+                remainingTries--;
+
+                Console.Error.WriteLine("Passwords do not match!\n" +
+                        "If this was an intentional password change, remove $LastPasswordHash in config.json");
+                Console.WriteLine("Remaining tries: " + remainingTries);
+            }
+            throw new Exception("Too many tries");
+
+        }
+        static string ReadPassword() {
             Console.WriteLine("Enter Password: ");
-            Console.Write(">>> ");
-            bool testing = false;
-            Password = testing ? "pass" : Console.ReadLine();
 
+            StringBuilder password = new();
+            ConsoleKeyInfo read = Console.ReadKey(true);
+            while (read.Key != ConsoleKey.Enter) {
+                password.Append(read.KeyChar);
+                read = Console.ReadKey(true);
+            }
+
+            return password.ToString();
+        }
+        static void PrepEnv() {
             ProgramConfig.Load(CONFIG_FILE_NAME);
 
             bool isRootFolder = ProgramConfig.get().IsThisRootFolder;
@@ -31,7 +70,7 @@
             } else {
                 Console.WriteLine("Enter Path (Must contain ./Original subdir) ): ");
                 Console.Write(">>> ");
-                ROOT_DIR = testing ? "B:/xyz" : Console.ReadLine();
+                ROOT_DIR = Console.ReadLine();
             }
 
             try {
@@ -41,6 +80,10 @@
             } catch (Exception ex) { throw ex.GetBaseException(); }
             if (!isRootFolder)
                 ProgramConfig.Load(FileUtils.CONFIG);
+
+            VerifyPassword();
+
+            FileCache.LoadAll();
         }
         static void Cleanup() {
             Console.WriteLine("Cleaning up work folders");
@@ -53,37 +96,78 @@
         static void Run(string[] args) {
             PrepEnv();
             string[] originalFiles = Directory.GetFiles(FileUtils.ORIGINAL_DIR, "", SearchOption.AllDirectories);
+            if (ProgramConfig.get().RandomizeFileOrder) {
+                new Random().Shuffle(originalFiles);
+            }
+
             foreach (string originalFile in originalFiles) {
                 try {
-                    FileStream read = File.OpenRead(originalFile);
-                    read.Close();
-                } catch (Exception _) {
-                    Console.WriteLine($"Cannot read {originalFile}. Skipping.");
-                }
-                string relativePath = Path.GetRelativePath(FileUtils.ORIGINAL_DIR, originalFile);
-                if (originalFile.EndsWith(".7z")) {
-                    new ReEncryptArchive(FileUtils.ORIGINAL_DIR, relativePath).Run();
-                } else {
-                    bool? shouldEncryptPre = ProgramConfig.get().ShouldEncryptStandaloneFiles;
-                    bool shouldEncrypt = shouldEncryptPre ?? true;
-                    if (!shouldEncryptPre.HasValue) {
-                        Console.WriteLine($"Encrypt {originalFile}? (y/n)");
-                        Console.Write(">>> ");
-                        shouldEncrypt = Console.ReadLine().Equals("y");
-                    }
-                    if (shouldEncrypt) {
-                        Encrypt encrypt = new(FileUtils.ORIGINAL_DIR, relativePath, true);
-                        if (!encrypt.ShouldSkipEncryption())
-                            encrypt.EncryptFile();
-                    } else {
-                        string skippedFile = FileUtils.SkippedArchivePath(relativePath);
-                        Console.WriteLine($"Copying {originalFile} to {skippedFile}");
-                        FileUtils.ParentMkDirs(skippedFile);
-                        File.Copy(originalFile, skippedFile, true);
-                    }
+                    TryFile(originalFile);
+                } catch (Exception e) {
+                    string error = $"Error with {originalFile}: {e.Message}";
+                    Console.Error.WriteLine(error);
+                    errors.Add(error);
                 }
             }
+
             Cleanup();
+        }
+        static void TryFile(string originalFile) {
+            try {
+                FileStream read = File.OpenRead(originalFile);
+                read.Close();
+            } catch {
+                throw new IOException($"Cannot read {originalFile}. Skipping.");
+            }
+            string relativePath = Path.GetRelativePath(FileUtils.ORIGINAL_DIR, originalFile);
+
+
+            Console.Write($"Starting {originalFile} ... ");
+            Console.Out.FlushAsync();
+
+            bool cached = FileCache.IsCached(originalFile, relativePath);
+            if (cached) {
+                Console.WriteLine($"Hit cache.");
+                return;
+            }
+
+
+
+            if (originalFile.EndsWith(".7z", StringComparison.InvariantCulture)) {
+                try {
+                    new ReEncryptArchive(FileUtils.ORIGINAL_DIR, relativePath).Run();
+                } catch (Exception e) {
+                    throw new IOException($"Cannot reencrypt - {e.Message}.");
+                }
+                FileCache.Save(originalFile, relativePath);
+                return;
+            }
+
+            bool? shouldEncryptPre = ProgramConfig.get().ShouldEncryptStandaloneFiles;
+            bool shouldEncrypt = shouldEncryptPre ?? true;
+
+            if (!shouldEncryptPre.HasValue) {
+                Console.WriteLine($"Encrypt {originalFile}? (y/n)");
+                Console.Write(">>> ");
+                shouldEncrypt = Console.ReadLine().Equals("y");
+            }
+            if (shouldEncrypt) {
+                Encrypt encrypt = new(FileUtils.ORIGINAL_DIR, relativePath, true);
+                if (encrypt.ShouldSkipEncryption(null))
+                    return;
+
+                try {
+                    encrypt.EncryptFile();
+                } catch (Exception e) {
+                    throw new IOException($"Cannot encrypt - {e.Message}.");
+                }
+                FileCache.Save(originalFile, relativePath);
+            } else {
+                string skippedFile = FileUtils.SkippedArchivePath(relativePath);
+                Console.WriteLine($"Copying {originalFile} to {skippedFile}");
+                FileUtils.ParentMkDirs(skippedFile);
+                File.Copy(originalFile, skippedFile, true);
+            }
         }
 
 
--- PassProtect7z/ProgramConfig.cs
+++ PassProtect7z/ProgramConfig.cs
@@ -1,22 +1,31 @@
 ﻿using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace PassProtect7z {
     class ProgramConfig {
         private static ProgramConfig instance;
+        private static string SaveFile = "";
 
+        public string[] IgnoreEncryptedPasswords = Array.Empty<string>();
         public bool? ShouldEncryptStandaloneFiles = null;
-        public bool SkipIntegrityCheck = false;
+        public bool AlwaysEncryptAgain = false;
         public DateTime SkipEncryptionTimestamp = DateTime.Now;
         public bool IsThisRootFolder = false;
+        public bool ShouldVerify7zContents = true;
+        public string? LastPasswordHash;
+        public bool RandomizeFileOrder = true;
+
         public static void Load(string file) {
             FileInfo config = new(file);
             if (!config.Exists) {
                 instance = new ProgramConfig();
             } else {
-                instance = JsonConvert.DeserializeObject<ProgramConfig>(File.ReadAllText(file)) ?? throw new Exception($"Error Parsing {FileUtils.CONFIG}");
+                instance = JsonConvert.DeserializeObject<ProgramConfig>(File.ReadAllText(file)) ??
+                    throw new Exception($"Error Parsing {FileUtils.CONFIG}");
             }
-            string obj = JsonConvert.SerializeObject(instance, Formatting.Indented);
-            File.WriteAllText(file, obj);
+            SaveFile = file;
+            Save();
         }
         public static ProgramConfig get() {
             return instance;
@@ -27,6 +36,27 @@ namespace PassProtect7z {
         public string SkipEncryptionTimestampFormat() {
             return SkipEncryptionTimestamp.ToString();
         }
+
+        public bool PasswordMatches(string newPassword) {
+            string newHash = HashSha256(newPassword);
+            if (this.LastPasswordHash == null) {
+                this.LastPasswordHash = newHash;
+                Save();
+            }
+            return this.LastPasswordHash == newHash;
+        }
+
+
+        internal static string HashSha256(string plaintext) {
+            byte[] data = Encoding.UTF8.GetBytes(plaintext);
+            byte[] hashBytes = SHA256.Create().ComputeHash(data);
+            return Convert.ToBase64String(hashBytes);
+        }
+        internal static void Save() {
+            string obj = JsonConvert.SerializeObject(instance, Formatting.Indented);
+            File.WriteAllText(SaveFile, obj);
+        }
+
     }
 
 }
